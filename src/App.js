7ac502a0599212413929f3378bafdcb727c790e0
HarshaSudheer <harsha.sudheer@thoughtworks.com>
2022-03-19 09:11:32 +0530

    Add. input field to get description from user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Tabs</h1>
+      <input type="text" maxLength="100" name="about" placeholder="oneline info for this tab"/><br />
     </div>
   );
 }
-
 export default App;
