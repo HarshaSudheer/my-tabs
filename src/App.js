182e92995d1c79429aac0a451bb1bc031a9cb226
HarshaSudheer <harsha.sudheer@thoughtworks.com>
2022-03-19 09:17:02 +0530

    Add. state for description from user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 const App = () => {
+  const [description, setDescription] = useState("");
+
   return (
     <div>
       <h1>My Tabs</h1>
-      <input type="text" maxLength="100" name="about" placeholder="oneline info for this tab"/><br />
+      <input type="text" maxLength="100" name="about" placeholder="oneline info for this tab" value={description} onChange={event => setDescription(event.target.value)}/><br />
     </div>
   );
 }
