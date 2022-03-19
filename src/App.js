ebb0dbe12f74afb655cf74a07892f46666b07691
HarshaSudheer <harsha.sudheer@thoughtworks.com>
2022-03-19 09:20:06 +0530

    Add. buttons for save and delete all

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
   const [description, setDescription] = useState("");
 
+  function handleSave() {
+
+  }
+
+  function handleDeleteAll() {
+
+  }
+
   return (
     <div>
       <h1>My Tabs</h1>
-      <input type="text" maxLength="100" name="about" placeholder="oneline info for this tab" value={description} onChange={event => setDescription(event.target.value)}/><br />
+      <input type="text" maxLength="100" name="about" placeholder="oneline info for this tab" value={description} onChange={event => setDescription(event.target.value)} /><br />
+      <button type="button" onClick={handleSave}>SAVE</button>
+      <button type="button" onClick={handleDeleteAll}>DELETE ALL</button>
     </div>
   );
 }
